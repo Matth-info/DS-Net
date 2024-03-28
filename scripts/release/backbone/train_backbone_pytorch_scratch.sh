b5485731ac870fea75a196037a94192ec1c4a4c9
Matthias Besnier <besnier.matthias@gmail.com>
2024-03-28 14:01:07 +0100

    Update train_backbone_pytorch_scratch.sh

--- scripts/release/backbone/train_backbone_pytorch_scratch.sh
+++ scripts/release/backbone/train_backbone_pytorch_scratch.sh
@@ -1,10 +1,10 @@
 ngpu=1
-tag=train_backbone_pytorch_dist
+tag=train_backbone_pytorch_dist_scratch
 
 python -m torch.distributed.launch --nproc_per_node=${ngpu} cfg_train.py \
     --tcp_port 12345 \
-    --ckpt_name 'polarOffSet_backbone_scratch.pth'
     --batch_size ${ngpu} \
+    --ckpt_name 'polarOffSet_backbone_scratch.pth' \
     --config cfgs/release/backbone.yaml \
     --tag ${tag} \
     --launcher pytorch
